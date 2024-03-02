edc81406b2593bb2cf0c05424796dae77d4760a0
alex <enna@mailbox.org>
2024-03-02 13:55:53 +0100

    bookPageTest

--- test/testMongo.js
+++ test/testMongo.js
@@ -1,17 +1,24 @@
 const mongo = require('../scripts/mongoclient')
 const assert = require('assert');
-
+const config = require('../scripts/config')
 async function run() {
-
 
     const client = await mongo.getClient().connect();
     async function pingTest() {
         return  await client.db("admin").command({ping: 1}) ? true : false;
-
     }
 
+    async function bookPageTest() {
+        const db = client.db(config.dbName)
+        const collection = db.collection(config.collectionName)
+        return true
+    }
+
+
+
     try {
-        assert(await pingTest()  )
+        assert(await pingTest())
+        assert(await bookPageTest())
 
     } finally {
         // Ensures that the client will close when you finish/error
