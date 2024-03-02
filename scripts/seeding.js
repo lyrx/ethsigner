fd8b5c6149ba30c0b0b0abd4807fbe98b66041b0
alex <enna@mailbox.org>
2024-03-02 13:51:46 +0100

    Refactoring tests

--- scripts/seeding.js
+++ scripts/seeding.js
@@ -3,17 +3,15 @@ const path = require('path');
 const {getClient} = require('./mongoclient'); // Adjust the path as necessary
 const xml2js = require('xml2js');
 const pandocutil = require('./pandocutil')
-
+const config = require('./config')
 // Database and Collection Names
-const dbName = 'DocumentLibrary';
-const collectionName = 'Ebooks';
 
 
 module.exports = {
     addEbookToDatabase: async function (ebookData) {
         const client = await getClient().connect()
-        const db = client.db(dbName)
-        const collection = db.collection(collectionName)
+        const db = client.db(config.dbName)
+        const collection = db.collection(config.collectionName)
 
         // await client.db("admin").command({ ping: 1 });
         await collection.insertOne(ebookData)
